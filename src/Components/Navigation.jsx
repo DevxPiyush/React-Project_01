4d9acb96f4eb2272a0f534323eb7a817325e260a
DevxPiyush <devxpiyush16.02@gmail.com>
2026-03-05 15:28:05 +0530

    Added title

--- src/Components/Navigation.jsx
+++ src/Components/Navigation.jsx
@@ -1,53 +1,64 @@
-import React, {useContext} from 'react'
-import {ProductContext} from "../utils/Context.jsx";
-import {Link, useLocation} from "react-router-dom";
+import React, { useContext } from "react";
+import { ProductContext } from "../utils/Context.jsx";
+import { Link, useLocation } from "react-router-dom";
 
 const Navigation = () => {
-    const [product] = useContext(ProductContext);
+  const [product] = useContext(ProductContext);
 
-    const {search, pathname} = useLocation()
+  const { search, pathname } = useLocation();
 
-    let distinct_category = product && product.reduce((acc,cv) => [...acc, cv.category], [])
-    distinct_category = [...new Set(distinct_category)]
-    // console.log(distinct_category);
+  let distinct_category =
+    product && product.reduce((acc, cv) => [...acc, cv.category], []);
+  distinct_category = [...new Set(distinct_category)];
+  // console.log(distinct_category);
 
-    const color = () => {
-        return `rgba(${Math.floor(Math.random()*255)},
-        ${Math.floor(Math.random()*255)},
-        ${Math.floor(Math.random()*255)},
-        ${Math.floor(Math.random()*255)}
-        )`
-    }
-    return (
-        <nav className="w-[18%] h-screen bg-white shadow-md fixed left-0 top-0 p-8 flex flex-col gap-8">
+  const color = () => {
+    return `rgba(${Math.floor(Math.random() * 255)},
+        ${Math.floor(Math.random() * 255)},
+        ${Math.floor(Math.random() * 255)},
+        ${Math.floor(Math.random() * 255)}
+        )`;
+  };
+  return (
+    <nav className="w-[18%] h-screen bg-white shadow-md fixed left-0 top-0 p-8 flex flex-col gap-8">
+      <h1 className="font-semibold text-3xl text-blue-600"> ShopFizz.app </h1>
 
-            <a
-                href="/create"
-                className="px-5 py-3 bg-blue-600 text-white text-center rounded-xl hover:bg-blue-500 transition"
-            >
-                Add New Product
-            </a>
+      <a
+        href="/create"
+        className="px-5 py-3 bg-blue-600 text-white text-center rounded-xl hover:bg-blue-500 transition"
+      >
+        Add New Product
+      </a>
 
-            <hr />
+      <hr />
 
-            <h1 className="text-xl font-semibold text-blue-700">
-                Category
-            </h1>
+      <h1 className="text-xl font-semibold text-blue-700">Category</h1>
 
-            <div className="flex flex-col gap-4">
-                {distinct_category.map((c,i) => (
-                    <Link key={i} to={`/?category=${c}`} className="flex items-center gap-2 text-gray-700 hover:text-blue-600 cursor-pointer">
-                       <span
-                           className="w-2 h-2 rounded-full"
-                           style={{ backgroundColor: color() }}
-                       ></span>
-                        {c}
-                    </Link>
-                ))}
-            </div>
-            {(pathname!= "/" || search.length>0) && <Link to={`/`} className={`px-3 py-3 bg-red-400 w-[40%] text-center text-white font-semibold rounded-full`}> Go Home </Link> }
-
-        </nav>
-    )
-}
-export default Navigation
+      <div className="flex flex-col gap-4">
+        {distinct_category.map((c, i) => (
+          <Link
+            key={i}
+            to={`/?category=${c}`}
+            className="flex items-center gap-2 text-gray-700 hover:text-blue-600 cursor-pointer"
+          >
+            <span
+              className="w-2 h-2 rounded-full"
+              style={{ backgroundColor: color() }}
+            ></span>
+            {c}
+          </Link>
+        ))}
+      </div>
+      {(pathname != "/" || search.length > 0) && (
+        <Link
+          to={`/`}
+          className={`px-3 py-3 bg-red-400 w-[40%] text-center text-white font-semibold rounded-full`}
+        >
+          {" "}
+          Go Home{" "}
+        </Link>
+      )}
+    </nav>
+  );
+};
+export default Navigation;
